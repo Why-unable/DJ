ce9cdb9adf53b64535c65f228728d5994131d563
Why-unable <quenlin166@gmail.com>
2023-10-06 09:16:54 +0800

    1006删除collect

--- my_blog/my_blog/wsgi.py
+++ my_blog/my_blog/wsgi.py
@@ -8,13 +8,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-print(sys.path)
-
-
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_blog.settings')
-
 application = get_wsgi_application()
